Fix sign ambiguity

get_masses_2.py
@@ -16,7 +16,7 @@ for galaxy in sorted(distances.keys()):
 	pfile = galaxy+'-gprof-lr.txt'
 	if not isfile(pfile): continue
 	vel, prof = np.loadtxt(pfile, usecols=(1,2), unpack=True)
-	total = np.sum(prof)*(vel[1]-vel[0])
+	total = np.sum(prof)*np.abs(vel[1]-vel[0])
 	mass = total * 2.36e5 * distance**2
 	for line in open('globprof-log.txt'):
 		if 'W50' in line and galaxy in line:

plot_globprofs.py
@@ -31,7 +31,7 @@ def get_widths(v, p, fit=False):
 		v20 = v[i20]
 		w50 = max(v50) - min(v50)
 		w20 = max(v20) - min(v20)
-	return w50, w20
+	return np.abs(w50), np.abs(w20)
 
 def get_galaxies():
 	cubelist = sorted(glob.glob('*-cube.fits'))